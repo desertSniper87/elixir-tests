4ec06d9f7f19b8cb07b6db8295ca01949481b631
desertsniper87 <torshobuet@gmail.com>
2024-07-11 00:35:05 +0600

    fantastic

--- caesar/test/caesar/cipher_test.exs
+++ caesar/test/caesar/cipher_test.exs
@@ -4,7 +4,19 @@ defmodule Caesar.CipherTest do
 
   import Caesar.Cipher
 
-  test "Caesar Encryption" do
+  test "Caesar Encryption small letter" do
     assert encrypt("abcd", 1) == "zabc"
+  end
+
+  test "Caesar Encryption space" do
+    assert encrypt("ab cd", 1) == "za bc"
+  end
+
+  test "Caesar Encryption capital letter" do
+    assert encrypt("ABCD", 1) == "ZABC"
+  end
+
+  test "Caesar Encryption mixed" do
+    assert encrypt("abCD", 1) == "zaBC"
   end
 end
