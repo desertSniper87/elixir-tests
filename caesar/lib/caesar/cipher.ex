9e12664d25b953499fba2550b8b67416d12ad694
desertsniper87 <torshobuet@gmail.com>
2024-07-11 11:16:29 +0600

    fantastic

--- caesar/lib/caesar/cipher.ex
+++ caesar/lib/caesar/cipher.ex
@@ -1,5 +1,20 @@
 defmodule Caesar.Cipher do
+  require Logger
+  @alphabet_size 26
+
+  @moduledoc """
+  GUGU GAGA
+  KAESAR SIPHER
+  """
+
+  @doc """
+  BUBU BABA
+  ENKRIPT
+  """
+
   def encrypt(msg, shift) do
+    Logger.debug("Enc -> #{msg} -> #{shift}")
+
     msg
     |> to_charlist()
     |> Enum.map(&shift_char(&1, shift))
@@ -15,8 +30,8 @@ defmodule Caesar.Cipher do
   end
 
   def calculate_mapping(base_letter, char, shift) do
-    normalize = &(&1 - 26)
-    shift_num = rem(shift, 26)
+    normalize = &(&1 - @alphabet_size)
+    shift_num = rem(shift, @alphabet_size)
     base_letter + rem(char - normalize.(base_letter) - shift_num, 26)
   end
 end
